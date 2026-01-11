44e6b0a299e0e628c617e5c3bf30461195de833b
Mahendra Nirmal <mahendranirmal00@gmail.com>
2026-01-11 08:28:01 +0530

    ViewModel Implementation

--- app/src/main/java/com/example/lunchtray/OrderViewmodel.kt
+++ app/src/main/java/com/example/lunchtray/OrderViewmodel.kt
@@ -1,4 +1,84 @@
 package com.example.lunchtray
 
-class OrderViewmodel {
+import androidx.lifecycle.ViewModel
+import com.example.lunchtray.data.OrderUIState
+import com.example.lunchtray.model.MenuItem
+import com.example.lunchtray.model.MenuItem.Accompaniment
+import com.example.lunchtray.model.MenuItem.SideDishMenu
+import com.example.lunchtray.model.MenuItem.EntreeMenu
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import java.text.NumberFormat
+
+class OrderViewmodel: ViewModel() {
+    private val taxRate = 0.08
+   private val _uiState = MutableStateFlow(OrderUIState())
+    val uiState : StateFlow<OrderUIState> = _uiState.asStateFlow()
+
+    fun  updateEntree(
+        entree  : EntreeMenu
+    ){
+
+        val prerviousEntree = uiState.value.entree
+        updateItem(
+            entree ,
+            prerviousEntree
+
+        )
+
+    }
+    fun updateSidedish(
+        sideDish : SideDishMenu
+    ){
+        val previousSideDishMenu = uiState.value.sideDish
+        updateItem(
+            sideDish ,
+            previousSideDishMenu
+        )
+
+    }
+    fun updateAccompaniment(
+        accompaniment :  Accompaniment
+    ){
+        val previousAccompaniment = uiState.value.accompaniment
+        updateItem(
+            accompaniment ,
+            previousAccompaniment
+            )
+
+    }
+    fun resetOrder(){
+
+        _uiState.value = OrderUIState()
+    }
+
+    private fun updateItem(
+        newItem : MenuItem,
+        previousItem: MenuItem?
+    ){
+        _uiState.update {
+            currentState ->
+            val previousItemPrice = previousItem?.price ?:0.0
+            val itemTotalPrice = currentState.itemTotalPrice - previousItemPrice +newItem.price
+            val tax = itemTotalPrice * taxRate
+
+
+            currentState.copy(
+                itemTotalPrice = itemTotalPrice,
+                orderTax = tax,
+                orderTotalPrice = itemTotalPrice + tax,
+                entree = if (newItem is EntreeMenu ) newItem else currentState.entree,
+                sideDish = if (newItem is SideDishMenu) newItem else currentState.sideDish,
+                accompaniment = if(newItem is Accompaniment) newItem else currentState.accompaniment
+
+            )
+        }
+    }
+
+    fun Double.formatPrice(): String {
+        return NumberFormat.getCurrencyInstance().format(this)
+    }
+
 }
--- app/src/main/java/com/example/lunchtray/data/OrderUIState.kt
+++ app/src/main/java/com/example/lunchtray/data/OrderUIState.kt
@@ -1,5 +1,14 @@
 package com.example.lunchtray.data
 
+import com.example.lunchtray.model.MenuItem
+
 data class OrderUIState(
- val name : String
+
+    val entree : MenuItem.EntreeMenu? = null,
+    val sideDish :  MenuItem.SideDishMenu? = null,
+    val accompaniment :  MenuItem.Accompaniment ? = null,
+    val itemTotalPrice : Double  = 0.0,
+    val orderTax :  Double  = 0.0,
+    val orderTotalPrice : Double  = 0.0
+
 )
